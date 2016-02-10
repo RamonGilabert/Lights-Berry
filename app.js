c58e906f29330283ea8ba27f437c3f50c7169af6
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-02-10 21:44:55 +0100

    Adds more files

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
+
+app.set('port', 6000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});

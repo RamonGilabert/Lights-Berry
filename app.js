238c3b6e3ad2fb25c83cbc3c55b7bd692b8edd33
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-02-13 21:26:53 +0100

    Adds the remote field

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 var databaseAddress = process.env.DATABASE_URL || 'postgres://localhost';
 var bookshelf = require('./app/classes/database.js')(databaseAddress);
+var io = require('socket.io-client');
+var socket = io.connect('https://http://lights-backend.herokuapp.com/', { reconnect: true });
 
 var Controller = require('./app/models/controller.js')(bookshelf);
 var Light = require('./app/models/light.js')(bookshelf);
 
 var control = require('./app/classes/control.js');
-
 
 app.set('port', 6000);
 app.set('views', __dirname + '/views');
@@ -16,8 +17,13 @@ app.set('view engine', 'ejs');
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 
-  control.checkFlow(bookshelf, Light, Controller)
-  .then(function(result) {
-    console.log(result)
-  });
+  // TODO: Uncomment those lines when working on that.
+  // control.checkFlow(bookshelf, Light, Controller)
+  // .then(function(result) {
+  //   console.log(result)
+  // });
+});
+
+socket.on('connect', function(socket) {
+  console.log('Values should be set to the Raspberry Pi now.');
 });

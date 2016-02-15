55675f2998447a497dc89699eae448894249c1c8
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-02-15 20:36:08 +0100

    Adds the library to manage the lights

---
+++ app/classes/berry.js
@@ -0,0 +1,27 @@
+/* The berry controller */
+
+var rpio = require('rpio');
+
+var pins = [11, 13, 15];
+var range = 1024;
+var clockDivider = 8;
+var interval = 5;
+
+for (pin in pins) {
+  rpio.open(pin, rpio.PWM);
+  rpio.pwmSetClockDivider(clockDivider);
+  rpio.pwmSetRange(pin, range);
+}
+
+var direction = 1;
+var data = 0;
+var pulse = setInterval(function() {
+        rpio.pwmSetData(pin, data);
+        if (data === 0) {
+          direction = 1;
+        } else if (data === max) {
+          direction = -1;
+        }
+
+        data += direction;
+}, interval, data, direction);

282afaaf29acc0e0ae411cd02316449e60a40f11
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-02-16 22:48:19 +0100

    Adds the code to pass the light

--- app/classes/berry.js
+++ app/classes/berry.js
@@ -1,6 +1,6 @@
 /* The berry controller */
 
-module.exports = function() {
+module.exports = function(light) {
   var rpio = require('rpio');
 
   var redPin = 12;
@@ -9,7 +9,6 @@ module.exports = function() {
   var pins = [redPin, greenPin, bluePin];
   var range = 255;
   var clockDivider = 8;
-  var interval = 1000;
 
   pins.forEach(function(pin) {
     rpio.open(pin, rpio.PWM);
@@ -17,13 +16,9 @@ module.exports = function() {
     rpio.pwmSetRange(pin, range);
   });
 
-  //rpio.write(12, rpio.HIGH);
-
-  rpio.pwmSetData(redPin, range);
-  rpio.pwmSetData(greenPin, range);
-  rpio.pwmSetData(bluePin, range);
-
-  process.stdin.resume();
+  rpio.pwmSetData(redPin, light.red * light.intensity * 255);
+  rpioGreen.pwmSetData(greenPin, light.green * light.intensity * 255);
+  rpioTwo.pwmSetData(bluePin, light.blue * light.intensity * 255);
 
   process.on('SIGINT', function() {
     pins.forEach(function(pin) { rpio.close(pin); });
